399114fd7951943598907404ef786daf1bbb6004
Beastie PC <gskondratenko@opora.org.ua>
2019-04-26 00:30:37 +0300

    Start scrape for bc

--- lib/get_mps.rb
+++ lib/get_mps.rb
@@ -2,10 +2,15 @@ require 'open-uri'
 require 'json'
 class GetMp
   def initialize
-    @data_hash = JSON.load(open('https://scrapervinnitsadeputy.herokuapp.com/'))
+    @data_hash = JSON.load(open('http://bcmp.oporaua.org/'))
   end
-  def serch_mp(short_name)
-   data = @data_hash.find {|k| k["short_name"] == short_name  }
-   return data["deputy_id"]
+  def serch_mp(full_name)
+     p full_name
+   if full_name == "Підпалий Сергій Миколайович"
+     return 6043
+   else
+     data = @data_hash.find {|k| k["full_name"] == full_name  }
+     return data["deputy_id"]
+   end
   end
 end
--- lib/scrape_all_votes.rb
+++ lib/scrape_all_votes.rb
@@ -4,6 +4,7 @@ require 'json'
 require 'date'
 require 'open-uri'
 require 'csv'
+require 'nokogiri'
 class GetAllVotes
    def initialize
      @all_file = get_all_file()
@@ -11,62 +12,70 @@ class GetAllVotes
    end
    def get_all_file
      hash = []
-     uri = "http://opendata.gov.ua/api/3/action/resource_search?query=name:%D0%93%D0%BE%D0%BB%D0%BE%D1%81%D1%83%D0%B2%D0%B0%D0%BD%D0%BD%D1%8F"
-     json = open(uri).read
-     hash_json = JSON.parse(json)
-     hash_json["result"]["results"].each do |f|
-       #p f["url"]
-       hash << { path: f["url"], last_modified: f["last_modified"], created: f["created"] }
-      end
+     url = "http://195.22.141.33/xml/"
+     page = Nokogiri::HTML(open(url, "User-Agent" => "HTTP_USER_AGENT:Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/534.13 (KHTML, like Gecko) Chrome/9.0.597.47"), nil, 'utf-8')
+     page.css('table tr').each do |tr|
+       next if tr.css('a').first.nil?
+       next if tr.css('a').first.text == "Name"  or tr.css('a').first.text =="Parent Directory"
+       hash << { path: tr.css('a')[0][:href], last_modified: tr.css('td')[2].text.strip }
+     end
      return hash
    end
   def get_all_votes
     @all_file.each do |f|
       update = UpdatePar.first(url: f[:path], last_modified: f[:last_modified])
       if update.nil?
-        read_file(f[:path], f[:created] )
+        read_file("http://195.22.141.33/xml/" + f[:path], f[:last_modified] )
         UpdatePar.create!(url: f[:path], last_modified: f[:last_modified])
       end
     end
   end
   def read_file(file, create)
-
     hash = []
-    CSV.foreach(open(file), {headers: true, :col_sep => ';', :row_sep => "\r\n", :encoding => 'windows-1251:utf-8'})  do |row|
-        h = []
-        row.each do |i|
-          if  i[0] == "id"
-            h2 = {}
-            h2[:number] = i[1]
-            h2[:date_caden] = DateTime.parse(create).strftime('%Y-%m-%d')
-            h << h2
-          elsif i[0] == "FullAskText"
-            h.last[:name] = i[1]
-            h.last[:voted] = []
-          elsif  i[0] == "result"
-            if i[1] == "РІШЕННЯ ПРИЙНЯТО"
-              h.last[:result] = "Прийнято"
-            else
-              h.last[:result] = "Не прийнято"
-            end
-          else
-            next if i[0].nil?
-            deputy = $all_mp.serch_mp(i[0].gsub(/\n/, ''))
-            vote = short_voted_result(i[1].strip)
-            h.last[:voted] << { voter_id: deputy, result: vote }
-          end
-        end
-      hash << h
+    absents = []
+    xml = Nokogiri::XML(open(file, "User-Agent" => "HTTP_USER_AGENT:Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/534.13 (KHTML, like Gecko) Chrome/9.0.597.47"), nil, 'cp1251')
+    xml.xpath('//Deputats/*').each do |mp|
+      if mp.at_xpath('Present').text == "Відсутній"
+        absents << $all_mp.serch_mp(mp.at_xpath('FIO').text)
+      end
+    end
+
+    xml.xpath('//Vopros').each do |v|
+     division = {}
+     division[:number] =  v.xpath('ID').text
+     date =  v.xpath('Voted').text
+     division[:date_caden] = DateTime.parse(date).strftime('%Y-%m-%d')
+     division[:date_vote] = DateTime.parse(date).strftime('%Y-%m-%d %H:%M:%s')
+     division[:name] = v.xpath('Long').text
+     if v.xpath('Result').text == "РІШЕННЯ ПРИЙНЯТО"
+       division[:result] = "Прийнято"
+     else
+       division[:result] = "Не прийнято"
+     end
+     division[:voted] = []
+     v.xpath('Poimen/Deputat').each do |r|
+       deputy = $all_mp.serch_mp(r.xpath('Name').text)
+       if r.xpath('Golos').text == "Н/Г"
+         if absents.include?(deputy)
+           vote = "absent"
+         else
+           vote = "not_voted"
+         end
+       else
+         vote = short_voted_result(r.xpath('Golos').text)
+       end
+       division[:voted] << { voter_id: deputy, result: vote }
+     end
+     hash << division
     end
     save_vote(hash)
-
   end
    def save_vote(hash)
      hash.each do |r|
 
-       event = VoteEvent.first(name: r[0][:name], number: r[0][:number], date_caden:  r[0][:date_caden], rada_id: 5, option: r[0][:result])
+       event = VoteEvent.first(name: r[:name], number: r[:number], date_caden: r[:date_caden], date_vote: r[:date_vote], rada_id: 8, option: r[:result])
           if event.nil?
-            events = VoteEvent.new(name: r[0][:name], number: r[0][:number], date_caden:  r[0][:date_caden], rada_id: 5, option: r[0][:result])
+            events = VoteEvent.new(name: r[:name], number: r[:number], date_caden:  r[:date_caden],date_vote: r[:date_vote], rada_id: 8, option: r[:result])
             events.date_created = Date.today
             events.save
           else
@@ -74,25 +83,21 @@ class GetAllVotes
             events.votes.destroy!
           end
 
-       r[0][:voted].each do |v|
+       r[:voted].each do |v|
            vote = events.votes.new
            vote.voter_id = v[:voter_id]
            vote.result = v[:result]
            vote.save
        end
-
      end
-
    end
   def short_voted_result(result)
     hash = {
-        "НЕ ГОЛОСУВАВ":  "not_voted",
-        ВІДСУТНІЙ: "absent",
-        ВІДСУТНЯ: "absent",
-        ПРОТИ:  "against",
-        ЗА: "aye",
-        УТРИМАВСЯ: "abstain",
-        УТРИМАЛАСЬ: "abstain"
+        "Н/Г":  "not_voted",
+        Відсутній: "absent",
+        Проти:  "against",
+        За: "aye",
+        "Утр.": "abstain",
     }
     hash[:"#{result.upcase}"]
   end
