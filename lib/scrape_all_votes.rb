e4b9e8d120201fb4cd3365ebc51d5d4171155f04
Beastie PC <gskondratenko@opora.org.ua>
2019-04-26 00:57:23 +0300

    Enable save

--- lib/scrape_all_votes.rb
+++ lib/scrape_all_votes.rb
@@ -76,9 +76,7 @@ class GetAllVotes
        event = VoteEvent.first(name: r[:name], number: r[:number], date_caden: r[:date_caden], date_vote: r[:date_vote], rada_id: 8, option: r[:result])
           if event.nil?
             p "Create"
-            events = VoteEvent.new(name: r[:name], number: r[:number], date_caden:  r[:date_caden],date_vote: r[:date_vote], rada_id: 8, option: r[:result])
-            events.date_created = Date.today
-            events.save
+            events = VoteEvent.create!(name: r[:name], number: r[:number], date_caden:  r[:date_caden],date_vote: r[:date_vote], rada_id: 8, option: r[:result], date_created: Date.today)
           else
             p "update"
             events = event
